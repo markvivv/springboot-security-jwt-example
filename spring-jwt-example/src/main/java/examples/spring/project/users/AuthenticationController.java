f5d860506f138d7311890e7dbba61f3a002c020f
xiesh <xiesh@51bsi.com>
2020-04-14 11:33:15 +0800

    增加返回token过期时间，过期时间使用jvm运行环境的时区格式化时间。

--- spring-jwt-example/src/main/java/examples/spring/project/users/AuthenticationController.java
+++ spring-jwt-example/src/main/java/examples/spring/project/users/AuthenticationController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +38,8 @@ public class AuthenticationController {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     @PostMapping(value = "/authenticate")
     public ResponseEntity authenticate(@Valid @RequestParam("user_name") @NotBlank(message = "用户名不能为空！") String username,
                                        @RequestParam("password") @NotBlank(message = "密码不能为空！") String password) {
@@ -48,6 +52,9 @@ public class AuthenticationController {
             Map<String, Object> model = new HashMap<>();
             model.put("user_name", username);
             model.put("token", token);
+            model.put("token_expiration", dateTimeFormatter.format(
+                    jwtTokenProvider.getTokenExpiration(token)
+                            .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()));
             return ok(Body.build().ok("登录成功", model));
         } catch (BadCredentialsException e) {
             return ok(Body.build().fail("账号或密码错误！"));
@@ -61,6 +68,9 @@ public class AuthenticationController {
         Map<String, Object> model = new HashMap<>();
         model.put("user_name", jwtTokenProvider.getUsername(newToken));
         model.put("token", newToken);
+        model.put("token_expiration", dateTimeFormatter.format(
+                jwtTokenProvider.getTokenExpiration(newToken)
+                        .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()));
         return ok(Body.build().ok("刷新token成功", model));
     }
 
