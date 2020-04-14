b996e0c9839f50e1f3d587f47ab612083f37b47c
xiesh <xiesh@51bsi.com>
2020-04-14 11:10:46 +0800

    字符串拼接使用编译器自动优化，不使用StringBuilder

--- spring-jwt-example/src/main/java/examples/spring/project/security/JwtUserDetailsService.java
+++ spring-jwt-example/src/main/java/examples/spring/project/security/JwtUserDetailsService.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.Transient;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -35,7 +34,7 @@ public class JwtUserDetailsService implements UserDetailsService {
             logger.info("登录用户信息：{}", user);
             return user;
         } catch (Exception e) {
-            String msg = new StringBuilder().append("Username: ").append(username).append(" not found").toString();
+            String msg = "Username: " + username + " not found";
             logger.error(msg, e);
             throw new UsernameNotFoundException(msg);
         }
