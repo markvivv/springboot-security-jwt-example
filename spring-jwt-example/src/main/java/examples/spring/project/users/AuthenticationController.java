aaae869db0e62d0f8dacc07771c33612fc094a9f
xiesh <xiesh@51bsi.com>
2020-04-14 09:01:56 +0800

    重命名，增加刷新API

--- spring-jwt-example/src/main/java/examples/spring/project/users/AuthenticationController.java
+++ spring-jwt-example/src/main/java/examples/spring/project/users/AuthenticationController.java
@@ -1,7 +1,6 @@
 package examples.spring.project.users;
 
 import examples.spring.project.Body;
-import examples.spring.project.security.JwtUserDetailsService;
 import examples.spring.project.security.jwt.JwtTokenProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
@@ -23,7 +23,8 @@ import java.util.Map;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
-public class LoginController {
+@RequestMapping(value = "/jwt")
+public class AuthenticationController {
 
     private Logger logger = LogManager.getLogger(getClass());
 
@@ -33,7 +34,7 @@ public class LoginController {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
-    @PostMapping(value="/authenticate")
+    @PostMapping(value = "/authenticate")
     public ResponseEntity authenticate(@Valid @RequestParam("user_name") @NotBlank(message = "用户名不能为空！") String username,
                                        @RequestParam("password") @NotBlank(message = "密码不能为空！") String password) {
         logger.debug("用户 {} 开始登录。", username);
@@ -49,6 +50,16 @@ public class LoginController {
         } catch (BadCredentialsException e) {
             return ok(Body.build().fail("账号或密码错误！"));
         }
+    }
+
+    @PostMapping(value="/refresh_token")
+    public ResponseEntity refreshToken(HttpServletRequest request) {
+        String currToken = jwtTokenProvider.resolveToken(request);
+        String newToken = jwtTokenProvider.refreshToken(currToken);
+        Map<String, Object> model = new HashMap<>();
+        model.put("user_name", jwtTokenProvider.getUsername(newToken));
+        model.put("token", newToken);
+        return ok(Body.build().ok("刷新token成功", model));
     }
 
     @GetMapping("/current_user")
